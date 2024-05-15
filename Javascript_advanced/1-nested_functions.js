822b39375dffc730e4a0c9b880614e59192d26f1
truuue <noahvernhet@gmail.com>
2024-05-15 15:28:20 +0200

    1. Closure Scope Chain

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,22 @@
+var globalVariable = "Welcome";
+
+function outer() {
+  alert(globalVariable);
+
+  var course = "Holberton";
+
+  function inner() {
+    alert(globalVariable + " " + course);
+
+    var exclamation = "!";
+
+    function inception() {
+      alert(globalVariable + " " + course + " " + exclamation);
+    }
+
+    inception();
+  }
+  inner();
+}
+
+outer();
