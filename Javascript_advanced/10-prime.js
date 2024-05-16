cf038ba008f393ff2a581d41a48b8cfea794486b
truuue <noahvernhet@gmail.com>
2024-05-17 00:06:19 +0200

    10. Execution stack & timing execution

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers() {
+  function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) if (num % i === 0) return false;
+    return num > 1;
+  }
+
+  return Array.from({ length: 99 }, function (_, i) {
+    return i + 2;
+  }).filter(isPrime).length;
+}
+
+const startTimer = performance.now();
+
+for (let exec = 0; exec <= 100; exec++) {
+  countPrimeNumbers();
+}
+
+const endTimer = performance.now();
+
+const runingTime = endTimer - startTimer;
+
+console.log(
+  "Execution time of calculating prime numbers 100 times was",
+  runingTime,
+  " milliseconds."
+);
