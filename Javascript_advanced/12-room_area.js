1da8bc8888daed3653bdfd0c7ae8c9cc0b4860a2
Noah <57148467+truuue@users.noreply.github.com>
2024-05-17 00:21:46 +0200

    Delete Javascript_advanced/12-room_area.js

--- Javascript_advanced/12-room_area.js
+++
@@ -1,9 +0,0 @@
-const roomDimensions = {
-  width: 50,
-  length: 100,
-  getArea: function () {
-    return this.width * this.length;
-  },
-};
-
-var boundGetArea = roomDimensions.getArea.bind(roomDimensions);
