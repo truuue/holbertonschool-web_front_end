a136fe2114bd572602b2063d6fdbe12ef87f7c4d
truuue <noahvernhet@gmail.com>
2024-05-24 10:03:46 +0200

    [1. Closure Scope Chain] Remove an useless space

--- Javascript_advanced/1-nested_functions.js
+++ Javascript_advanced/1-nested_functions.js
@@ -11,7 +11,7 @@ function outer() {
     var exclamation = "!";
 
     function inception() {
-      alert(globalVariable + " " + course + " " + exclamation);
+      alert(globalVariable + " " + course + exclamation);
     }
 
     inception();
