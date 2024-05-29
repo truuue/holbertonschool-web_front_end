3465940c2e8a2706bd29f38384139df837c4ad2b
truuue <noahvernhet@gmail.com>
2024-05-29 11:21:03 +0200

    [2. Read cookie] Finished the task

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -17,11 +17,30 @@ function setCookies() {
 function showCookies() {
   // Crée un élément de paragraphe
   const paragraph = document.createElement("p");
-  // Récupère tous les cookies
-  const cookies = document.cookie;
+  // Récupère la valeur du cookie "email"
+  const email = getCookie("email");
+  // Récupère la valeur du cookie "firstname"
+  const firstname = getCookie("firstname");
 
   // Définit le contenu du paragraphe avec les cookies
-  paragraph.innerHTML = `Cookies: ${cookies}`;
+  paragraph.innerHTML = `Email: ${email} - Firstname: ${firstname}`;
   // Ajoute le paragraphe au corps du document
   document.body.appendChild(paragraph);
+}
+
+function getCookie(name) {
+  // Sépare tous les cookies en utilisant le point-virgule et l'espace comme séparateurs
+  const cookies = document.cookie.split("; ");
+  // Parcourt tous les cookies
+  for (let i = 0; i < cookies.length; i++) {
+    // Sépare chaque cookie en utilisant le signe égal comme séparateur
+    const cookie = cookies[i].split("=");
+    // Vérifie si le nom du cookie correspond à celui recherché
+    if (cookie[0] === name) {
+      // Retourne la valeur du cookie
+      return cookie[1];
+    }
+  }
+  // Retourne une chaîne vide si le cookie n'est pas trouvé
+  return "";
 }
